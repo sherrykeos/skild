2702af99fdfc3f7916eb28d9e5f8e817fea2659a
sherry <shaj7492@gmail.com>
2026-08-28 22:45:19 +0530

    feat: setup engagement routes in central routing

--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { router as authRouter } from "../modules/auth/index.js";
 import skillRoutes from "../modules/skills/skill.routes.js";
 import marketplaceRoutes from "../modules/marketplace/marketplace.routes.js";
+import engagementRoutes from "../modules/engagement/engagement.routes.js";
 
 const router = Router();
 
@@ -14,5 +15,8 @@ router.use("/skills", skillRoutes);
 
 //marketplace routes
 router.use("/marketplace", marketplaceRoutes);
+
+//engagement routes
+router.use("/", engagementRoutes);
 
 export default router;
