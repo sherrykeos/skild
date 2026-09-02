ae45b6218fff04b8542eaa03c32ec684769634f5
sherry <shaj7492@gmail.com>
2026-09-02 23:25:10 +0530

    react forntend removed

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -4,7 +4,10 @@ import pg from "pg";
 
 const connectionString = process.env.DATABASE_URL;
 
-const pool = new pg.Pool({ connectionString });
+const pool = new pg.Pool({
+  connectionString,
+  ssl: { rejectUnauthorized: false },
+});
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
